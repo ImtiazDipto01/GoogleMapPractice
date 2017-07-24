5bd7df8c7491fd021fa9e56d9c68a7f8ae6b4377
imtiaz <programmer.imtiaz@gmail.com>
2017-07-23 19:10:42 -0700

    Add- adding custom marker

--- app/src/main/java/com/example/dipto/googlemappractice/MainActivity.java
+++ app/src/main/java/com/example/dipto/googlemappractice/MainActivity.java
@@ -7,12 +7,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mymap ;
+    LatLng office = new LatLng(23.750101, 90.392779) ;
+    LatLng ulab = new LatLng(23.740823, 90.374510) ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         LatLng myhome = new LatLng(23.7578944,90.4390535) ;
         mymap.addMarker(new MarkerOptions().position(myhome).title("10 tala building")) ;
-        mymap.moveCamera(CameraUpdateFactory.newLatLngZoom(myhome, 15f));
+        mymap.moveCamera(CameraUpdateFactory.newLatLngZoom(myhome, 12f));
+
+
+        mymap.addMarker(new MarkerOptions()
+                .position(office)
+                .title("NextDot")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.new_marker)));
+        mymap.moveCamera(CameraUpdateFactory.newLatLngZoom(office, 12f));
+
+
+        mymap.addMarker(new MarkerOptions()
+                .position(ulab)
+                .title("University Of Liberal Arts")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.new_marker_2)));
+        mymap.moveCamera(CameraUpdateFactory.newLatLngZoom(ulab, 12f));
     }
 }
